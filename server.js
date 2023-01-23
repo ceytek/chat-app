60526ca8df11faaa2d3fd4ed469fda2e11df6c2f
ceytek <ceyhuntekin85@gmail.com>
2023-01-23 21:43:48 +0300

    server.js emit mesaj eklendi,main.js dizin changed

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const path = require("path");
 const http = require('http');
-const socket = require('socket.io');
+const socketio = require('socket.io');
 const express = require('express');
 
 require('dotenv').config()
 const PORT = 3000 || process.env.PORT;  
 
-
-
 const app = express();
 const server = http.createServer(app);
-server.listen(PORT , () => console.log(`Server is running on port ${PORT}`));
+
+
 app.use(express.static(path.join(__dirname, "public")));
 
 const Redis = require('ioredis');
@@ -20,13 +19,18 @@ const redis = new Redis({
 })
 
 
-const io = socket(server);
+const io = socketio(server);
 
 //Run when client connect
 io.on('connect', socket =>{
-    console.log("New User came")
+    console.log("New User came");
+
+    socket.emit("message", "Welcome to Chat")
 });
 
 io.on('disconnect', socket =>{
     console.log("Disconnect")
 })
+
+
+server.listen(PORT , () => console.log(`Server is running on port ${PORT}`));

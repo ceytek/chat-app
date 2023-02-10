c61155e378315370f24ded55c052bb4600beaa74
ceytek <ceyhuntekin85@gmail.com>
2023-02-10 08:50:23 +0300

    user ve room ayarlandı

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const express = require('express');
 const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser} = require('./utils/users');
 
 require('dotenv').config()
 const PORT = 3001 || process.env.PORT;  
@@ -25,18 +26,28 @@ const io = socketio(server);
 
 //Run when client connect
 io.on('connect', socket =>{
-    console.log("New User came");
-
-
-    socket.emit("message", formatMessage(username ,'Welcome to Chat'));
-    socket.broadcast.emit("message",formatMessage(username ,'New User came' ));
+    socket.on('joinRoom', ({username, room})=>{
+    const user =userJoin(socket.id, username, room)
+    socket.join(user.room)
+       
+    socket.emit('message', formatMessage(username ,'Welcome to Chat'));
+   
+    socket.broadcast
+     .to(user.room)
+     .emit('message',
+     formatMessage(username ,`${user.username} came`)
+    );
+    
+    socket.on('chatMessage',msg =>{
+        io.emit('message',formatMessage('User' , msg));
+      });
     socket.on('disconnect', () =>{
         io.emit('message', formatMessage(username ,'Kullanıcı ayrıldı.'));
     });
 
-    socket.on('chatMessage',msg =>{
-      io.emit('message',formatMessage('User' , msg));
-    });
+    })
+
+
 });
 
 

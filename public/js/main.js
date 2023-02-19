b0bbec100af0682b1fa56f7c6d317b52b45011e2
ceytek <ceyhuntekin85@gmail.com>
2023-02-19 13:52:54 +0300

    roomlist front-end

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,31 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages= document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
+// Get username and room from URL
 const {username,room} = Qs.parse(location.search, {
     ignoreQueryPrefix: true
 });
 
 const socket = io();
 
+// Join chatroom
 socket.emit('joinRoom', {username,room});
 
+// Get room and users
+socket.on('roomUsers', ({room, users}) =>{
+   outputRoomName(room);
+   outputUsers(users);
+});
+
+// Message from server
 socket.on("message", message => {
     outputMessage(message);
     chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
+// Scroll down
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const msg = e.target.elements.msg.value;
@@ -23,6 +35,7 @@ chatForm.addEventListener('submit', (e) => {
 
 });
 
+// Output message to DOM
 function outputMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
@@ -31,4 +44,14 @@ function outputMessage(message) {
         ${message.text}
     </p>`;
      document.querySelector('.chat-messages').appendChild(div)
+}
+
+// Add room name to DOM
+function outputRoomName(room){
+    roomName.innerHTML = room;
+}
+
+// Add users to DOM
+function outputUsers(users){
+
 }
